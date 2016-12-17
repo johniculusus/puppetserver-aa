24692f49c28fba9d34d66d4225fc936fa264578e
johniculusus <johndiaz.developer@gmail.com>
2016-12-17 00:30:29 -0500

    Added a node for non proxy node

--- code/environments/production/manifests/site.pp
+++ code/environments/production/manifests/site.pp
@@ -1,4 +1,4 @@
-node 'puppetc.asiapacific.hpqcorp.net' {
+node 'puppetc.asiapacific.hpqcorp.net' { # This node is connected to HPE network / with proxy
 	include openssh::server
 	
 	file { '/tmp/testmaster.txt':
@@ -8,4 +8,16 @@ node 'puppetc.asiapacific.hpqcorp.net' {
 		mode => '777',
 		source => "puppet:///modules/wew/test2.txt"
 	}
+}
+
+node 'puppetc' { # This node is not connected to HPE network / no proxy
+        include openssh::server
+
+        file { '/tmp/testmaster.txt':
+                ensure => present,
+                owner => root,
+                group => root,
+                mode => '777',
+                source => "puppet:///modules/wew/test2.txt"
+        }
 }
